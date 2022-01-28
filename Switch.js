650f50431a21dedf1c83b5a08b290bd93764411e
Jamshid Gayrt <jamshidbekgayratjanov@Jamshidbeks-MacBook-Pro.local>
2022-01-28 09:39:11 +0000

    Create Switch.js

---
+++ Switch.js
@@ -0,0 +1,26 @@
+console.log("\n----------Switch Case----------")
+let language = "FR" // it, fr, ....
+
+let greeting
+if (language === "EN") {
+    greeting = "Hello!"
+} else if (language === "FR") {
+    greeting = "Salut"
+} else if (language === "ES") {
+    greeting = "Hola"
+ {
+    greeting = "Hello!"
+}
+}
+console.log(greeting)
+
+switch (language) {
+    case "EN": greeting = "Hello!"; break
+    case"IT": greeting = "Ciao!"; break
+    case "FR": greeting = "Salut!"; break
+    case "ES": greeting = "Hola!"; break
+    case "PT": greeting = "OLá!"; break
+    case "DE": greeting = "Hallo!"; break
+    default: greeting = "Hello!"; break
+    }
+    console. log(greeting)
